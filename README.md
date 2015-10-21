33ce4562a809295584f9c4b6b6b19f518f1edea7
Corbin Murrow <corbin.murrow@gmail.com>
2015-10-21 16:12:07 -0500

    Created Initial Files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Team BOEing's Libraries
+
+Team BOEing's Library Code for redesigning the BOEbot to use the ATmega328 chip instead of the BASIC Stamp microprocessor. 
---
+++ User_Code_v1.0/User_Code_v1.0.ino
@@ -0,0 +1,21 @@
+/*
+ * User Code Library v1.0
+ * Author: Corbin Murrow
+ * Date: 21 October 2015
+ * Version: 1.0
+ * 
+ * <description>
+ * 
+ * ======= VERSION HISTORY =======
+ * Version 1.0: Created Project - CM - 21 October 2015
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
